b52f89292387441d44c0e0f634be7b9c35ebcad1
Cameron Robinson <cameron@ckrobinson.net>
2023-07-12 16:25:06 -0700

    Added decodable data to model.

--- Stocks/Model/PortfolioModel.swift
+++ Stocks/Model/PortfolioModel.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 struct PortfolioResponse: Decodable {
-    
+    let stocks: [Stock]
+}
+
+struct Stock: Decodable {
+    let ticker: String
+    let name: String
+    let currency: String
+    let currentPriceCents: Int
+    let quantity: Int?
+    let currentPriceTimestamp: Int
+
+    enum CodingKeys: String, CodingKey {
+        case ticker
+        case name
+        case currency
+        case currentPriceCents = "current_price_cents"
+        case quantity
+        case currentPriceTimestamp = "current_price_timestamp"
+    }
 }
