d4ecd07d4a6954f6f0bf56214d50680177673873
Cameron Robinson <cameron@ckrobinson.net>
2023-07-12 16:16:01 -0700

    Initial commit.

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Cameron on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
