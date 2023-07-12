52101069564e8f0d7c78a87a45eee58f07af6080
Cameron Robinson <cameron@ckrobinson.net>
2023-07-12 16:32:30 -0700

    Updated view model to fetch data.

--- Stocks/ViewModel/ContentViewModel.swift
+++ Stocks/ViewModel/ContentViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var stocks: [Stock] = [];
+    let networkService: NetworkService = NetworkService();
+    
+    @MainActor func fetchData() {
+        
+        Task {
+            do {
+                let data = try await self.networkService.fetchPortfolio();
+                print(data);
+            }
+            catch {
+                print(error.localizedDescription);
+            }
+        }
+    }
+}
