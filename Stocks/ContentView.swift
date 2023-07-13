8cf0640dd2815fbe64092a19ef52e62c4c800eb3
Cameron Robinson <cameron@ckrobinson.net>
2023-07-13 16:36:00 -0700

    Added simple card view for each stock.

--- Stocks/ContentView.swift
+++ Stocks/ContentView.swift
@@ -15,13 +15,49 @@ struct ContentView: View {
         ScrollView {
             ForEach(self.viewModel.stocks, id: \.self) { stock in
                 
-                Text(stock.ticker)
+                StockCardView(stockData: stock)
+                    .padding(.bottom)
+                
             }
         }
         .padding()
         .onAppear() {
             viewModel.fetchData()
         }
+    }
+}
+
+struct StockCardView: View {
+    
+    let stockData: Stock;
+    var body: some View {
+        
+        HStack {
+            VStack(alignment: .leading) {
+                Text(stockData.ticker)
+                Text(stockData.name)
+                    .font(.footnote)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            VStack {
+                if( Int.random(in: 0...1) == 0) {
+                    Text("$\(stockData.currentPriceCents)")
+                        .foregroundColor(.red)
+                        .shadow(color: .black, radius: 0.1)
+                }
+                else {
+                    Text("$\(stockData.currentPriceCents)")
+                        .foregroundColor(.green)
+                        .shadow(color: .black, radius: 0.1)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(Color(UIColor.systemGray5))
+        .cornerRadius(15)
     }
 }
 
