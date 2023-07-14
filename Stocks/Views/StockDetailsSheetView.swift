f90409888696e38ae0fc7d840cc9086597e17703
Cameron Robinson <cameron@ckrobinson.net>
2023-07-14 15:37:39 -0700

    Added new sheet view.

---
+++ Stocks/Views/StockDetailsSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  StockDetailsSheetView.swift
+//  Stocks
+//
+//  Created by Cameron on 7/14/23.
+//
+
+import SwiftUI
+
+struct StockDetailsSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StockDetailsSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockDetailsSheetView()
+    }
+}
