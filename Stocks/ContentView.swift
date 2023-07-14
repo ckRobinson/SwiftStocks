93235c70aaf66c04488d5f5d4f9e3e499796404b
Cameron Robinson <cameron@ckrobinson.net>
2023-07-14 12:02:57 -0700

    Refactored stocks card view.

--- Stocks/ContentView.swift
+++ Stocks/ContentView.swift
@@ -34,6 +34,7 @@ struct ContentView: View {
                 }
             }
             .padding()
+            .navigationTitle("Stocks")
         }
         .onAppear() {
             viewModel.fetchData()
@@ -54,40 +55,6 @@ struct ContentView: View {
     
     var errorLoading: some View {
         Text("Could not load data. Please try again later.")
-    }
-}
-
-struct StockCardView: View {
-    
-    let stockData: Stock;
-    var body: some View {
-        
-        HStack {
-            VStack(alignment: .leading) {
-                Text(stockData.ticker)
-                Text(stockData.name)
-                    .font(.footnote)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Spacer()
-            VStack {
-                if( Int.random(in: 0...1) == 0) {
-                    Text("$\(stockData.currentPriceCents)")
-                        .foregroundColor(.red)
-                        .shadow(color: .black, radius: 0.1)
-                }
-                else {
-                    Text("$\(stockData.currentPriceCents)")
-                        .foregroundColor(.green)
-                        .shadow(color: .black, radius: 0.1)
-                }
-            }
-        }
-        .frame(maxWidth: .infinity)
-        .padding()
-        .background(Color(UIColor.systemGray5))
-        .cornerRadius(15)
     }
 }
 
---
+++ Stocks/Views/StockCardView.swift
@@ -0,0 +1,48 @@
+//
+//  StockCardView.swift
+//  Stocks
+//
+//  Created by Cameron on 7/14/23.
+//
+
+import SwiftUI
+
+struct StockCardView: View {
+    
+    let stockData: Stock;
+    var body: some View {
+        
+        HStack {
+            VStack(alignment: .leading) {
+                Text(stockData.ticker)
+                Text(stockData.name)
+                    .font(.footnote)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            VStack {
+                if( Int.random(in: 0...1) == 0) {
+                    Text("$\(stockData.currentPriceCents)")
+                        .foregroundColor(.red)
+                        .shadow(color: .black, radius: 0.1)
+                }
+                else {
+                    Text("$\(stockData.currentPriceCents)")
+                        .foregroundColor(.green)
+                        .shadow(color: .black, radius: 0.1)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(Color(UIColor.systemGray5))
+        .cornerRadius(15)
+    }
+}
+
+//struct StockCardView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        StockCardView()
+//    }
+//}
