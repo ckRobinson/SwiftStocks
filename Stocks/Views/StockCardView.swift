ad9b2325a387b75d0c80a4aa3987effcb32d4bc5
Cameron Robinson <cameron@ckrobinson.net>
2023-07-14 15:59:55 -0700

    Updated card view.

--- Stocks/Views/StockCardView.swift
+++ Stocks/Views/StockCardView.swift
@@ -7,44 +7,27 @@
 
 import SwiftUI
 
-enum StockPriceState {
-    case rising;
-    case falling;
-}
-
-private class StockCardViewModel {
-    let stockData: Stock;
-    let stockPriceString: String;
-    let currencySymbol: String;
-
-    init(stockData: Stock) {
-        self.stockData = stockData;
-        self.stockPriceString = String(format: "%0.2f", Float(stockData.currentPriceCents) / 100.0);
-        self.currencySymbol = "$";
-    }
-}
-
 struct StockCardView: View {
     
-    private let viewModel: StockCardViewModel
-    init(stockData: Stock) {
-        self.viewModel = StockCardViewModel(stockData: stockData);
+    let stock: Stock
+    init(stock: Stock) {
+        self.stock = stock;
     }
     
     var body: some View {
         
         HStack {
             VStack(alignment: .leading) {
-                Text(viewModel.stockData.ticker)
+                Text(stock.ticker)
                     .fontWeight(.bold)
-                Text(viewModel.stockData.name)
+                Text(stock.name)
                     .font(.footnote)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             
             Spacer()
 
-            Text("\(viewModel.currencySymbol)\(viewModel.stockPriceString)")
+            Text(stock.priceStringWithCurrency)
                 .shadow(color: .black, radius: 0.1)
             
             Image(systemName: "chevron.forward")
@@ -58,6 +41,6 @@ struct StockCardView: View {
 
 struct StockCardView_Previews: PreviewProvider {
     static var previews: some View {
-        StockCardView(stockData: Stock.mockData)
+        StockCardView(stock: Stock.mockData)
     }
 }
